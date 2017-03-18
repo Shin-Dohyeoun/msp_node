c020326b6f804914ccb16ac3770e922429ad414e
ShinDohyeoun <sdh9367@gmail.com>
2017-03-18 10:53:49 +0900

    change port

--- app.js
+++ app.js
@@ -19,4 +19,15 @@ app.use('/', index);
 app.use('/chat', chat);
 
 var server = http.createServer(app);
-server.listen(3000);
+server.listen(80);
+
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection',function(socket){
+    socket.emit('toclient',{msg:'Welcome !'});
+    socket.on('fromclient',function(data){
+        socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+        socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
+        console.log('Message from client :'+data.msg);
+    })
+});
